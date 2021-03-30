004bd8ff60217261e9fc8ad8df554b7269a44c89
Egor Klimov <egardoz01@gmail.com>
2021-03-30 13:53:57 +0300

    first algo added

---
+++ GraphicsAlgosLaba/MainForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace GraphicsAlgosLaba
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1000, 600);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(992, 574);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Отрезки с целыми координатами";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Location = new System.Drawing.Point(9, 7);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(813, 561);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(992, 574);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Отрезки с не целыми координатами";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(828, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Начало ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(831, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Конец";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 606);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "MainForm";
+            this.Text = "Лаба по компьютерной графике, Климов";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GraphicsAlgosLaba/MainForm.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicsAlgosLaba
+{
+    public partial class MainForm : Form
+    {
+
+        private bool is_begin;
+        private bool is_paint;
+        private Point p_begin;
+        private Point p_end;
+        private Panel drawind_panel;
+        public MainForm()
+        {
+            InitializeComponent();
+            InitLinesWithIntChords();
+        }
+
+        private void Plot(Graphics g, int x, int y)
+        {
+            g.DrawRectangle(new Pen(new SolidBrush(Color.Black)), x,y,1,1);
+        }
+
+        #region Lines_Int_Chords
+
+        private void InitLinesWithIntChords()
+        {
+            is_paint = false;
+            is_begin = true;
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex == 0)
+                InitLinesWithIntChords();
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (is_begin)
+            {
+                is_paint = false;
+                is_begin = false;
+                p_begin = e.Location;
+            }
+            else
+            {
+                p_end = e.Location;
+                label1.Text = $"Начало=({p_begin.X}, {p_begin.Y})";
+                label2.Text = $"Конец=({p_end.X}, {p_end.Y})";
+                is_begin = true;
+                is_paint = true;
+                panel1.Refresh();
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs pea)
+        {
+            if (is_paint)
+            {
+                //e.Graphics.DrawLine(new Pen(new SolidBrush(Color.Black)),p_begin, p_end);
+
+                int x = 0;
+                int y = 0;
+                int a = p_end.X - p_begin.X;
+                int b = p_end.Y - p_begin.Y;
+                int x_mnoj = 1, y_mnoj=1;
+                if (a < 0)
+                {
+                    a = -a;
+                    x_mnoj = -1;
+                }
+                if (b < 0)
+                {
+                    b = -b;
+                    y_mnoj = -1;
+                }
+                if (a > b)
+                {
+                    int e = 2 * b - a;
+                    int des = 2 * b;
+                    int ded = 2 * b - 2 * a;
+                    while (x < a)
+                    {
+
+                        Plot(pea.Graphics, x * x_mnoj + p_begin.X, y * y_mnoj + p_begin.Y);
+
+                        if (e > 0)
+                        {
+                            x++;
+                            y++;
+                            e += ded;
+                        }
+                        else
+                        {
+                            x++;
+                            e += des;
+                        }
+                    }
+                }
+                else
+                {
+                    int e = 2 * a - b;
+                    int des = 2 * a;
+                    int ded = 2 * a - 2 * b;
+                    while (x < a)
+                    {
+
+                        Plot(pea.Graphics, x * x_mnoj + p_begin.X, y * y_mnoj + p_begin.Y);
+
+                        if (e > 0)
+                        {
+                            x++;
+                            y++;
+                            e += ded;
+                        }
+                        else
+                        {
+                            y++;
+                            e += des;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        #endregion
+    }
+}
